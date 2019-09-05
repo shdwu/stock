48f0ddfcfe1c1b7ceb8d1cdad5c43ce215177f9a
yy <yy@gmail.com>
2019-09-05 17:53:49 +0800

    c server

---
+++ server.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet.h>
+#define PORT 12333
+
+int main(int argc, char *argv[])
