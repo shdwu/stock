fe66457c8e08855d4cfd57aff01b6b31d5ce77fe
shdwu <shdwu91@gmail.com>
2019-09-19 08:26:32 +0800

    curl add

---
+++ src/curl_http_get.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void) {
+  CURL *curl;
+  CURLcode res;
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "http://hq.sinajs.cn/list=s_sz002594");
+    curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, (long)CURL_HTTP_VERSION_2);
+    res = curl_easy_perform(curl);
+    if(res != CURLE_OK) {
+      fprintf(stderr, "curl failed %s\n", curl_easy_strerror(res));
+      curl_easy_cleanup(curl);
+    }
+  }
+  return 0;
+}
